88bd6c7bbef8a37a289b314096149a2b85243ec2
Matt Fellenz <matt@felle.nz>
2022-02-21 16:03:28 -0800

    Add and use Prettier for formatting

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const Crawler = require('crawler');
-const { generateImage } = require('./imageGeneration');
+const Crawler = require("crawler");
+const { generateImage } = require("./imageGeneration");
 
 exports.generateImage = function (url, pathToSave) {
-  // make into promise
-  return new Promise((resolve, reject) => {
-    const c = new Crawler({
-      maxConnections: 10,
-      async callback(error, res, done) {
-        if (error) {
-          reject(error);
-        } else {
-          const { $ } = res;
-          const fullTitle = $('title').text();
-          const title = fullTitle.split(':')[0].trim();
-          const subreddit = fullTitle.split(':')[1].trim();
-          await generateImage(title, subreddit, pathToSave);
-          resolve();
-        }
-        done();
-      },
-    });
-    c.queue(url.trim());
-  });
+	// make into promise
+	return new Promise((resolve, reject) => {
+		const c = new Crawler({
+			maxConnections: 10,
+			async callback(error, res, done) {
+				if (error) {
+					reject(error);
+				} else {
+					const { $ } = res;
+					const fullTitle = $("title").text();
+					const title = fullTitle.split(":")[0].trim();
+					const subreddit = fullTitle.split(":")[1].trim();
+					await generateImage(title, subreddit, pathToSave);
+					resolve();
+				}
+				done();
+			},
+		});
+		c.queue(url.trim());
+	});
 };
--- testing.js
+++ testing.js
@@ -1,3 +1,6 @@
-const { generateImage } = require('./index');
+const { generateImage } = require("./index");
 
-generateImage('https://www.reddit.com/r/worldnews/comments/sy2slo/russian_president_vladimir_putin_signed_a_decree/', './testing.png');
+generateImage(
+	"https://www.reddit.com/r/worldnews/comments/sy2slo/russian_president_vladimir_putin_signed_a_decree/",
+	"./testing.png"
+);
